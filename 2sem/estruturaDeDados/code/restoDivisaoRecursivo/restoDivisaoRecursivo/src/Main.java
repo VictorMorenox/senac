b885f93e7107338ad5c5adf9d83c79a3f65d0e0a
eopit <pedro.barricelli@gmail.com>
2020-03-05 01:27:24 -0300

    finished restoDivisaoRecursivo

--- 2sem/estruturaDeDados/code/restoDivisaoRecursivo/restoDivisaoRecursivo/src/Main.java
+++ 2sem/estruturaDeDados/code/restoDivisaoRecursivo/restoDivisaoRecursivo/src/Main.java
@@ -1,9 +1,18 @@
 public class Main {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    System.out.println(div(62, 99));
+  }
 
+  public static int div(int num, int div) {
+    if (sub(num, div) < div) {
+      return sub(num, div);
+    } else {
+      return div(sub(num, div), div);
     }
+  }
+
+  public static int sub(int first, int second) {
+    return first - second;
+  }
 }
-// JEITO DE FAZER
-// 4 - 2 -> resto 0
-// 2 - 2 -> resto 0
