a71e055860d28f5f9cdbf99af20359bc044d0ed6
eopit <pedro.barricelli@gmail.com>
2020-03-05 01:50:43 -0300

    Simplfied restoDivisaoRecursivo excluding else statement and beggining of fibonacci

---
+++ 2sem/estruturaDeDados/code/fibonacciRecursivo/fibonacciRecursivo/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        fibonacciRecursivo;
+    }
+
+    public static void fibonacciRecursivo(int max) {
+        if (max )
+    }
+}
--- 2sem/estruturaDeDados/code/restoDivisaoRecursivo/restoDivisaoRecursivo/src/Main.java
+++ 2sem/estruturaDeDados/code/restoDivisaoRecursivo/restoDivisaoRecursivo/src/Main.java
@@ -2,14 +2,14 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println(div(62, 99));
+    System.out.println(62 % 99);
   }
 
   public static int div(int num, int div) {
     if (sub(num, div) < div) {
       return sub(num, div);
-    } else {
-      return div(sub(num, div), div);
     }
+    return div(sub(num, div), div);
   }
 
   public static int sub(int first, int second) {
