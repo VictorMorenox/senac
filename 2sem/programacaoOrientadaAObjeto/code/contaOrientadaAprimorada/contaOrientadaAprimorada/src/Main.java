d584e1a47e784bc583b3d76013c822050c0ed62c
eopit <pedro.barricelli@gmail.com>
2020-03-05 21:10:33 -0300

    saving intellij crash

---
+++ 2sem/programacaoOrientadaAObjeto/code/contaOrientadaAprimorada/contaOrientadaAprimorada/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+  static Scanner sc = new Scanner(System.in);
+  static int opcao = 0;
+  static boolean passou = false;
+
+  public static void main(String[] args) {
+
+    sc.close();
+  }
+}
